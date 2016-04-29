d2d79bcaf6dbdf4ce934e8e858374819362e9184
aftab90 <aftabonquora@gmail.com>
2016-04-29 21:44:06 +0300

    starting indexcheckbox.php

--- helloworlds/fread.php
+++ helloworlds/fread.php
@@ -3,7 +3,7 @@ ob_start("ob_gzhandler");
 
 $url='https://en.wikipedia.org/wiki/List_of_Malayalam_films_of_2016';
 $url = 'http://www.livelifedrive.com/';
-$url = 'https://kat.cr/usearch/Monsoon%20Mangoes%20malayalam/';
+$url = 'compress.zlib://https://kat.cr/usearch/Monsoon%20Mangoes%20malayalam/';
 $handle = fopen($url, "r");
 
 if ($handle)
