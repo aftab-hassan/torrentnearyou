137e93d4819ca4b1a9e93583efad2c2c4bf02524
aftab90 <aftabonquora@gmail.com>
2016-05-01 13:42:12 +0300

    addinh hardcoded checkbox debug

--- php/indexdynamicdropdown.php
+++ php/indexdynamicdropdown.php
@@ -125,6 +125,7 @@ if(isset($_GET['languagedropdown']) && isset($_GET['yeardropdown']))
     {
         // error opening the file.
         echo "could not open the wikipedia URL!";
+        echo "could not find any ". $_GET['languagedropdown']." movies for the year ".$_GET['yeardropdown']." !";
     }
     fclose($handle);
 //    print_r($movienamearray);echo "</br>";echo "</br>";
