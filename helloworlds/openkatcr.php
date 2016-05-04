09881778bd826f55717fcfe4fd834ee42c7aadc0
aftab90 <aftabonquora@gmail.com>
2016-05-04 11:31:23 +0300

    addinh hardcoded checkbox debug

--- helloworlds/openkatcr.php
+++ helloworlds/openkatcr.php
@@ -10,18 +10,27 @@
 </html>
 
 <?php
-    $url = "compress.zlib://https://kat.cr/usearch/Monsoon%20Mangoes%20malayalam/";
+//    $url = "compress.zlib://https://kat.cr/usearch/Monsoon%20Mangoes%20malayalam/";
     $url = "https://kat.cr/usearch/Monsoon%20Mangoes%20malayalam/";
-    $handle = fopen($url, "r");
-    if($handle)
-    {
-        /* iterating to find the sizes of the torrents */
-        while (($line = fgets($handle)) !== false)
-        {
-            echo $line;
-        }
-    }
-    else{
-        echo "could not open the URL!";
-    }
+//    $handle = fopen($url, "r");
+//    if($handle)
+//    {
+//        /* iterating to find the sizes of the torrents */
+//        while (($line = fgets($handle)) !== false)
+//        {
+//            echo $line;
+//        }
+//    }
+//    else{
+//        echo "could not open the URL!";
+//    }
+
+$ch = curl_init();
+curl_setopt_array($ch, array(
+    CURLOPT_URL => $url
+, CURLOPT_HEADER => 0
+, CURLOPT_RETURNTRANSFER => 1
+, CURLOPT_ENCODING => 'gzip'
+));
+echo curl_exec($ch);
 ?>
