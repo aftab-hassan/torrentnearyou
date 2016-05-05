62b3ffbbdaa160ec5f85ae8217af7fe3355f9ef8
aftab90 <aftabonquora@gmail.com>
2016-05-05 22:56:22 +0300

    addinh hardcoded checkbox debug

--- php/index.php
+++ php/index.php
@@ -187,34 +187,32 @@ if(isset($_GET['language']) && isset($_GET['year']))
     $pattern_torrent_notpresent = "Nothing found!";
     $torrentlinkarray = array();
 
-//    for($i = 0;$i < count($movienamearray);$i++)
-//    {
-//        echo "<tr>"."<td>".$i."</td>"."<td>".$movienamearray[$i]."</td>"."<td>".""."</td>"."</tr>";
-//
-//        $myfile = fopen("/var/www/data/fileonserver".$_GET['randomNumber']."txt", "w") or die("Unable to open file!");
-//        $txt = "now at movie no.".$i;
-//        fwrite($myfile, $txt);
-//        fclose($myfile);
-//
-//        $url = $base.str_replace(" ","%20",$movienamearray[$i])."%20".$_GET['year']."%20".$_GET['language'];
-//
-//        $handle = fopen($url, "r");
-//        if ($handle)
-//        {
-//            array_push($torrentlinkarray,$url);
-//        }
-//        else
-//        {
-//            // error opening the file.
-//            array_push($torrentlinkarray,"404");
-//        }
-//        fclose($handle);
-//    }
-//    $myfile = fopen("/var/www/data/fileonserver".$_GET['randomNumber']."txt", "w") or die("Unable to open file!");
-//    $txt = "end of data";
-//    fwrite($myfile, $txt);
-//    fclose($myfile);
-//    //print_r($torrentlinkarray);
+    for($i = 0;$i < count($movienamearray);$i++)
+    {
+        $myfile = fopen("/var/www/data/fileonserver".$_GET['randomNumber']."txt", "w") or die("Unable to open file!");
+        $txt = "now at movie no.".$i;
+        fwrite($myfile, $txt);
+        fclose($myfile);
+
+        $url = $base.str_replace(" ","%20",$movienamearray[$i])."%20".$_GET['year']."%20".$_GET['language'];
+
+        $handle = fopen($url, "r");
+        if ($handle)
+        {
+            array_push($torrentlinkarray,$url);
+        }
+        else
+        {
+            // error opening the file.
+            array_push($torrentlinkarray,"404");
+        }
+        fclose($handle);
+    }
+    $myfile = fopen("/var/www/data/fileonserver".$_GET['randomNumber']."txt", "w") or die("Unable to open file!");
+    $txt = "end of data";
+    fwrite($myfile, $txt);
+    fclose($myfile);
+    //print_r($torrentlinkarray);
 
     /* Table is already created after wiki found movies. Only javascript edits are made to the table */
 }
