86d193fc618e587f461871e08ff1b25195dcdc2e
aftab90 <aftabonquora@gmail.com>
2016-05-05 21:12:07 +0300

    addinh hardcoded checkbox debug

--- php/iretriever.php
+++ php/iretriever.php
@@ -8,7 +8,7 @@
 
 $fileName = "/var/www/data/fileonserver".$_GET['randomNumber']."txt";
 //$fileContents = file_get_contents("/var/www/data/fileonserver".$_GET['randomNumber']."txt", "w", true);
-$fileContents = file_get_contents($fileName, "w", true);
-//echo $fileName;
-echo $fileContents;
+//$fileContents = file_get_contents($fileName, "w", true);
+echo $fileName;
+//echo $fileContents;
 ?>
