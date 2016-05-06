73d396f22f30dc97efa30b2c0d3dbaa09405b22d
aftab90 <aftabonquora@gmail.com>
2016-05-06 22:54:17 +0300

    addinh hardcoded checkbox debug

--- php/index.php
+++ php/index.php
@@ -70,10 +70,12 @@ include('simple_html_dom.php');
 
 /* gets the data from a URL */
 function get_data($url) {
+    $userAgent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)';
     $ch = curl_init();
     $timeout = 5;
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
     $data = curl_exec($ch);
     curl_close($ch);
