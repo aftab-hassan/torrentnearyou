9542cf9381dbc36d1f513d68fe68a66679574ff9
aftab90 <aftabonquora@gmail.com>
2016-05-06 14:48:22 +0300

    addinh hardcoded checkbox debug

--- php/safe.php
+++ php/safe.php
@@ -204,14 +204,17 @@ if(isset($_GET['languagedropdown']) && isset($_GET['yeardropdown']))
             {
                 $data = GetBetween("title","</a></i></td>",$line);
                 $moviename = substr($data, strpos($data, ">") + 1);
-                array_push($movienamearray, $moviename);
+
+                /* otherwise the table formatting gets affected */
+                if(strlen($moviename) < 50)
+                    array_push($movienamearray, $moviename);
 
                 $moviecount++;;
                 $i++;
             }
 
             // process the line read.
-            if ( ($moviecount > 0) && (strpos($line, $pattern_ending) !== false) )
+            if ( ($_GET['languagedropdown'] == "malayalam") && ($moviecount > 0) && (strpos($line, $pattern_ending) !== false) )
             {
                 break;
             }
