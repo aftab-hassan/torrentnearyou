a7075a1f51387975550e2729d86bb272488150cf
aftab90 <aftabonquora@gmail.com>
2016-05-07 08:54:09 +0300

    addinh hardcoded checkbox debug

---
+++ php/post.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aftab
+ * Date: 5/7/2016
+ * Time: 8:48 AM
+ */
+
+$statusArray = array();
+$allURLs = array("https://kat.cr/usearch/life%20of%20pi/","https://en.wikipedia.org/wiki/Basketball","https://kat.cr/usearch/life%20of%20pi/","https://en.wikipedia.org/wiki/Basketball","https://kat.cr/usearch/life%20of%20pi/");
+
+for($i = 0;$i < count($allURLs) ; $i++)
+{
+    $handle = fopen($allURLs[$i], "r");
+    if ($handle)
+    {
+        // error opening the file.
+        array_push($statusArray,"able to open the page");
+    }
+    else
+    {
+        // error opening the file.
+        array_push($statusArray,"404");
+    }
+    fclose($handle);
+}
+
+print($statusArray);
+?>
