ae83d6066657fad0ea6da6edb1c2595841ab3e86
aftab90 <aftabonquora@gmail.com>
2016-05-13 16:08:32 +0300

    rewrote index.php as thin client

--- php/populate.php
+++ php/populate.php
@@ -235,13 +235,13 @@ function GetBetween($var1="",$var2="",$pool){
 }
 
 /* Remmeber, the whole point of this is to generate the table code */
-//$languagearray = array('malayalam','hindi','english','tamil','telugu','kannada','tagalog');
-$languagearray = array('english');
+$languagearray = array('malayalam','hindi','english','tamil','telugu','kannada','tagalog');
+//$languagearray = array('english');
 for($lang = 0;$lang < count($languagearray);$lang++)
 {
     $language = $languagearray[$lang];
-//    for($year = 2010;$year <= date("Y");$year++)
-    for($year = 2016;$year <= 2016;$year++)
+    for($year = 2010;$year <= date("Y");$year++)
+//    for($year = 2016;$year <= 2016;$year++)
     {
         echo "Processing...".$language."_".$year."</br>";
 
